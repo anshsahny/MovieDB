512d88a8fd266cc527b740fa13104fc24a778006
Ansh Sahny <anshsahny@gmail.com>
2021-09-05 18:55:00 -0400

    inital commit

---
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './MovieThumb.css'
+
+const MovieThumb = ({ image, movieId, clickable, movieName }) => {
+    return (
+        <div className="rmdb-moviethumb">
+            {clickable ? 
+                <Link to={{pathname: `/${movieId}`, movieName: `${movieName}`}}>
+                    <img src={image} alt="moviethumb"/>
+                </Link>
+            : <img src={image} alt="moviethumb"/>}   
+        </div>
+    )
+}
+
+export default MovieThumb
